0b35e3443784bb2ac7ad36c4eb3f996d26e5dee0
Aayush <rg103436@gmail.com>
2026-05-23 07:51:38 +0545

    i added a script in pipe and other

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,4 +1,7 @@
-extends Node2D
+extends Node
+
+@export var pipe_scene : PackedScene
+
 
 var game_running : bool
 var game_over : bool
@@ -18,6 +21,7 @@ const PIPE_RANGE : int = 200
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_window().size
+	ground_height = $Ground.get_node("Sprite2D").texture.get_height()
 	new_game()
 
 func new_game():
@@ -25,6 +29,8 @@ func new_game():
 	game_over = false
 	score = 0
 	scroll = 0
+	pipes.clear()
+	generate_pipes()
 	$birds.reset()
 	
 func _input(event):
@@ -42,13 +48,32 @@ func start_game():
 	game_running = true
 	$birds.flying = true
 	$birds.flap()
+	$PipeTimer.start()
 
 
 func _process(delta: float) -> void:
 	if game_running:
 		scroll += SCROLL_SPEED 
-		
 		if scroll >= screen_size.x:
 			scroll = 0 
-			
 		$Ground.position.x = -scroll
+		for pipe in pipes:
+			pipe.position.x -= SCROLL_SPEED
+
+
+func _on_pipe_timer_timeout() -> void:
+	generate_pipes()
+	
+func generate_pipes():
+	var pipe = pipe_scene.instantiate()
+	pipe.position.x = screen_size.x + PIPE_DELAY
+	pipe.position.y = (screen_size.y - ground_height) / 2 + randi_range(-PIPE_RANGE, PIPE_RANGE)
+	pipe.hit.connect(bird_hit)
+	add_child(pipe)
+	pipes.append(pipe)
+	
+func bird_hit():
+	game_over = true
+	game_running = false
+	$PipeTimer.stop()
+	$Bird.falling = true
